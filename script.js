de425c40860ad30d8a1076cc296376ed325a44d9
Aliaksei <alxqwerty@gmail.com>
2022-09-29 22:42:41 +0300

    New commit accoring to home task.

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 "use strict";
 
-//1
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-
-//2
 const personalMovieDb = {
     count: numberOfFilms,
     movies: {},
@@ -13,58 +10,31 @@ const personalMovieDb = {
     private: false
 };
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ""),
+          b = prompt('Насколько оцените его?', "");
+
+    if (a != null && b != "null" && a != "" && b != "" && a.length < 50) {
+        personalMovieDb.movies[a] = b;   
+        console.log('done');
+    }
+    else {
+        i--;
+        console.log('error');
+    }
+}
 
 if (personalMovieDb.count >= 0 && personalMovieDb.count < 10) {
-    const optionA = alert("Просмотрено довольно мало фильмов.");
+    console.log("Просмотрено довольно мало фильмов.");
 } 
 else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
-    const optionB = alert("Вы классический зритель.");
+    console.log("Вы классический зритель.");
 }
 else if (personalMovieDb.count >= 30) {
-    const optionC = alert("Вы киноман.");
+    console.log("Вы киноман.");
 }
 else {
-    const optionD = alert("Произошла ошибка.");
+    console.log("Произошла ошибка.");
 }
-
-//3 
-let i = 0;
-let recentFilms = [];
-let recentRatings = [];
-
-do {
-    recentFilms[i] = prompt('Один из последних просмотренных фильмов?', "");
-
-    while (recentFilms[i] == '' || recentFilms[i].length > 50) {
-        recentFilms[i] = prompt('Один из последних просмотренных фильмов?', "");
-    }
-
-    recentRatings[i] = prompt('Насколько оцените его?', "");
-
-    while (recentRatings[i] == '' || recentRatings[i].length > 50) {
-        recentRatings[i] = prompt('Насколько оцените его?', "");
-    }
-    personalMovieDb.movies[recentFilms[i]] = recentRatings[i];
-    i++;
-} while (i < 2);
-
-// for (let i = 0; i < 2; i++) {
-//     recentFilms[i] = prompt('Один из последних просмотренных фильмов?', "");
-
-//     if (recentFilms[i] == ''|| recentFilms[i].length > 50) {
-//         recentFilms[i] = prompt('Один из последних просмотренных фильмов?', "");
-//         i--;
-//         continue;
-//     }
-
-//     for (let j = 0; j < 2; j++) {
-//         recentRatings[j] = prompt('Насколько оцените его?', "");
-
-//         if (recentRatings[j] == '' || recentRatings[j] > 50) {
-//             recentRatings[j] = prompt('Насколько оцените его?', "");
-//             j--;
-//         }
-//     }
-// }
 
 console.log(personalMovieDb);
